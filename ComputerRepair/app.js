b53d38f7339a163fb9ccc112c83c8d2cff1d7cba
C K Siew <ck_siew@hotmail.com>
2018-04-29 13:04:46 +0800

    Complete ComputerRepairs

--- ComputerRepair/app.js
+++ ComputerRepair/app.js
@@ -11,11 +11,43 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',function(req,res){
-    res.render('index',{title:"Welcome"});
+    res.render('index',{title:"Computer Not Working?"});
 });
 app.get('/about', function(req,res){
     res.render('about');
-})
+});
+app.get('/contact',function(req,res){
+    res.render('contact');
+});
+app.post('/contact/send', function(req,res){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: 'garyngreig@gmail.com',
+            pass: ''
+        }
+    });
+    var mailOptions = {
+        from: 'Gary Greig <garyngreig@gmail.com>',
+        to: 'gauravg@packtpub.com',
+        subject: 'Website Submission',
+        text: 'You have a submission with the following details... Name: '+ req.body.name + 'Email: ' +
+        req.body.email + 'Message: ' + req.body.message,
+        html: '<p>You have a submission with the following details...</p></ul><li>' + req.body.name + '</li><li>Email: ' + req.body.email + '</li></li>Message:' +
+        req.body.message+'</li></ul>'
+
+    };
+
+    transporter.sendMail(mailOptions,function(error,info){
+        if (error){
+            console.log(error);
+            res.redirect('/');
+        } else {
+            console.log('Message Send: ' + info.response);
+            res.redirect('/');
+        }
+    });
+});
 app.listen(3000);
 console.log('Server is running on port 3000');
 
